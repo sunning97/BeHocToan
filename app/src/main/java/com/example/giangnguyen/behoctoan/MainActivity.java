a113e67c5fb7294403b176c513e9350129cdf33c
sunning97 <mrcatbro97@gmail.com>
2017-08-14 00:13:04 +0700

    remove sound

--- app/src/main/java/com/example/giangnguyen/behoctoan/MainActivity.java
+++ app/src/main/java/com/example/giangnguyen/behoctoan/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.giangnguyen.behoctoan;
 
 import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -15,11 +18,13 @@ import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
+    final String DATA = "data";
+    final String HIGHT_SCORE = "hight score";
     int a;
     int b;
     int c;
     int score = 0;
-    int curr =100;
+    int curr = 100;
 
 
     TextView Txt_score, cauhoi;
@@ -28,12 +33,11 @@ public class MainActivity extends AppCompatActivity {
     RelativeLayout bg;
 
 
-
-    CountDownTimer countDownTimer = new CountDownTimer(1800,10) {
+    CountDownTimer countDownTimer = new CountDownTimer(1800, 10) {
         @Override
         public void onTick(long l) {
-            if(curr>0) {
-                curr --;
+            if (curr > 0) {
+                curr--;
                 progressBar.setProgress(curr);
             }
         }
@@ -45,13 +49,14 @@ public class MainActivity extends AppCompatActivity {
     };
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        final MediaPlayer songtrue = MediaPlayer.create(this, R.raw.coin);
+        final MediaPlayer songfalse = MediaPlayer.create(this, R.raw.game_over);
         Imag();
+        randomColor();
         PlayDialog();
 
 
@@ -63,7 +68,6 @@ public class MainActivity extends AppCompatActivity {
                     score++;
                     Txt_score.setText("" + score);
                     countDownTimer.cancel();
-                    trueSong();
                     load();
 
                 } else {
@@ -80,7 +84,6 @@ public class MainActivity extends AppCompatActivity {
                     score++;
                     Txt_score.setText("" + score);
                     countDownTimer.cancel();
-                    trueSong();
 
                     load();
 
@@ -96,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
     private void PlayDialog() {
 
         final Dialog PlayDialog = new Dialog(MainActivity.this);
+        PlayDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
         PlayDialog.setContentView(R.layout.playlayout);
         PlayDialog.setCancelable(false);
         ImageButton Play = (ImageButton) PlayDialog.findViewById(R.id.playImg1);
@@ -113,9 +117,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void GameoverDialog() {
         countDownTimer.cancel();
-        gameOverSong();
+        addData();
         final Dialog GameOverDialog = new Dialog(MainActivity.this);
+        GameOverDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
         GameOverDialog.setContentView(R.layout.game_over_dialog);
+        TextView scoree = (TextView) GameOverDialog.findViewById(R.id.scoree);
+        scoree.setText("" + score);
         ImageButton Play = (ImageButton) GameOverDialog.findViewById(R.id.playImg2);
         GameOverDialog.setCancelable(false);
         GameOverDialog.show();
@@ -135,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void load() {
-        curr =100;
+        curr = 100;
         Random random = new Random();
         a = random.nextInt(10) + 1;
         b = random.nextInt(10) + 1;
@@ -159,22 +166,27 @@ public class MainActivity extends AppCompatActivity {
         bg = (RelativeLayout) findViewById(R.id.bacground);
     }
 
-    private void gameOverSong()
-    {
-        MediaPlayer song = MediaPlayer.create(MainActivity.this, R.raw.game_over);
-        song.start();
-    }
-
-    private void trueSong()
-    {
-        MediaPlayer song = MediaPlayer.create(MainActivity.this, R.raw.coin);
-        song.start();
-    }
-
-    public void randomColor()
-    {
+    public void randomColor() {
         int[] androidColors = getResources().getIntArray(R.array.androidcolors);
         int randomAndroidColor = androidColors[new Random().nextInt(androidColors.length)];
         bg.setBackgroundColor(randomAndroidColor);
+    }
+
+    private int readData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(DATA, Context.MODE_PRIVATE);
+        int score = sharedPreferences.getInt(HIGHT_SCORE, 20);
+        return score;
+    }
+
+    private void addData() {
+        //if (score > readData()) {
+            SharedPreferences sharedPreferences = getSharedPreferences(DATA, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+            editor.putInt(HIGHT_SCORE, score);
+            editor.apply();
+        //}
+
+
     }
 }
